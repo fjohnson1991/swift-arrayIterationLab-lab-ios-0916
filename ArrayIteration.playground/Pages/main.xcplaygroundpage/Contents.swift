13ca93380dcd70210b7d062a7a6525075f23aa85
fjohnson1991 <felicity.johnson.mail@gmail.com>
2016-09-09 06:01:07 -0400

    commit

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,8 +13,7 @@
 /*: question1
  ### 1. You're building an app and want to store all of the ingredients added to a cart. How would you represent this cart in code? The ingredients are "Chips", "Salsa", "Guacamole", "Red wine". Explicitly mark the content of the cart as strings.
  */
-// write your code here
-
+var cart: [String] = ["Chips", "Salsa", "Guacamole", "Red Wine"]
 
 
 
@@ -23,7 +22,7 @@
 /*: question2
  ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code? Explicitly mark the content of the cart as numbers.
  */
-// write your code here
+var num: [Int] = [1,2,3,4,5,6,7,8,9,10]
 
 
 
@@ -34,8 +33,8 @@
 /*: question3
  ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible.
  */
-// write your code here
 
+print(num)
 
 
 
@@ -45,7 +44,10 @@
 /*: question4
  ### 4. Take the list of shopping cart items you created in question 1 and print their values in the most efficient way possible. Prefix each item in the array with a string of text.
  */
-// write your code here
+for item in cart {
+    
+    print("I must remember to buy \(item)")
+}
 
 
 
@@ -56,9 +58,18 @@
 /*: question5
  ### 5. Take the list of shopping cart items you created in question 1 and print their values by passing each item in list to a function to be printed.
  */
-// write your code here
+func cartFunc(cart: Array<String>) {
+    
+    var index = 0
+    
+    for item in cart {
+        
+        print("\(index). \(item)")
+        index++
+    }
+}
 
-
+cartFunc(cart)
 
 
 
@@ -67,21 +78,55 @@
 /*: question6
  ### 4. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
  */
-// write your code here
+var names: [String] = ["Ver", "Tony", "James", "Sarah", "Linda", "Michael"]
+
+func welcome(names: Array<String>) -> String {
+    
+    var greeting = ""
+    
+    for name in names {
+        
+        if name == "Michael" {
+            
+            greeting = "Top of the morning Michael!"
+            
+        } else {
+            
+            greeting = "Good morning \(name)"
+        }
+    
+    }
+    
+    return greeting
+}
 
 
-
+welcome(names)
 
 
 
 /*: question7
  ### 7. Create a function that takes an array of Ints (40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49) and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
-// write your code here
+var numInt: [Int] = [40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49]
+
+func intArray (num: Array<Int>) -> Array<Int> {
+    
+    var lessThanFifty : Array<Int> = []
+    
+    for int in numInt {
+        
+        if int < 50 {
+            
+            lessThanFifty.append(int)
+        }
+    }
+    
+    return lessThanFifty
+}
 
 
-
-
+intArray(numInt)
 
 
 
